dd4f7ac3ee93ee8bc1454313e2ff986fe1c2659c
neolao <contact@neolao.com>
2016-08-04 12:30:33 +0200

    update

---
+++ monitoring/monitoring-available-upgrade.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+count=$(aptitude search '~U' | wc -l)
+list=$(apt list --upgradable)
+
+
+curl "http://my.url/core/api/jeeApi.php?apikey=1234&type=httpRemoteEvent&eqId=1&value=$count"
+#curl --data-urlencode "value=$list" "http://jeedom.url/core/api/jeeApi.php?apikey=1234&type=httpRemoteEvent&eqId=40" > /dev/null 2>&1
