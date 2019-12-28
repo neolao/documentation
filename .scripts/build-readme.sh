6b75ed2263b3e526c9107a283fe5257ff7424f19
neolao <contact@neolao.com>
2019-12-28 00:52:26 +0000

    test

--- .scripts/build-readme.sh
+++ .scripts/build-readme.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-find . -name "*.md" -print
+cat .scripts/README.header
+find . -name "*.md" -execdir sh -c 'filename=${0##*/}; printf "%s [%s](%s)\n" "-" "${filename%.*}" "$filename"' {} ';'
+cat .scripts/README.footer
+
